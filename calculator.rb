623fbaf629d61fc352b2f81e52ba8fb8eab698b1
Tim Rinkerman <tim.rinkerman98@gmail.com>
2020-06-10 19:59:13 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,7 +5,7 @@ second_number = 3
 
 sum = first_number + second_number
 
-diffrence = first_number - second_number
+difference = first_number - second_number
 
 product = first_number * second_number
 
